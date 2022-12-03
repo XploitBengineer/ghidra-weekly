844e3fe3a92df4aef88f115184d85b5c3ca3f6f1
XploitBengineer <110530254+XploitBengineer@users.noreply.github.com>
2022-12-03 18:53:25 +0000

    Added bash scripts

---
+++ build_ghidra.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+build_ghidra()
+{
+    add-apt-repository ppa:openjdk-r/ppa
+    add-apt-repository ppa:cwchien/gradle
+    apt update
+    apt install -y default-jre gradle-ppa # https://github.com/gradle/gradle/issues/773
+    apt install openjdk-11-jdk
+    apt install openjdk-11-jre
+    
+    git clone https://github.com/NationalSecurityAgency/ghidra.git
+    cd ghidra
+    gradle buildGhidra
+
+}
+
+updated_last_day=$(curl -s https://api.github.com/repos/NationalSecurityAgency/ghidra/commits/master | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+
+if [ "$updated_last_day" -gt "0" ]
+then
+    build_ghidra
+fi
+
