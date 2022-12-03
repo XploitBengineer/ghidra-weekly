de506024c4799c2cfdc9e4fd9de3a947661640b7
XploitBengineer <110530254+XploitBengineer@users.noreply.github.com>
2022-12-03 19:00:38 +0000

    Update build_ghidra.sh

--- build_ghidra.sh
+++ build_ghidra.sh
@@ -12,6 +12,7 @@ build_ghidra()
     
     git clone https://github.com/NationalSecurityAgency/ghidra.git
     cd ghidra
+    gradle --init-script gradle/support/fetchDependencies.gradle init
     gradle buildGhidra
 
 }
